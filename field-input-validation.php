ee1c241eb895e0bcf45a2850463f1cd4ce58824e
Chandra Bhushan Sharma <chandrabhu24@gmail.com>
2019-12-19 14:52:32 +0530

    Update field-input-validation.php

--- field-input-validation.php
+++ field-input-validation.php
@@ -1,4 +1,10 @@
 <?php
+//reporting errors in php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 
 function escape_data($data){
 //Database Connection
